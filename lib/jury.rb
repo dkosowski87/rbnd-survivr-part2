10f249914f692a5ad27f3115a52765dc2ff7cdb8
Damian Kosowski <kosowski.d@gmail.com>
2016-06-17 17:51:03 +0200

    Implemented jury class

---
+++ lib/jury.rb
@@ -0,0 +1,32 @@
+class Jury
+	attr_accessor :members
+
+	def initialize
+		@members = []		
+	end
+
+	def add_member(contestant)
+		@members.push contestant
+	end
+
+	def cast_votes(finalists)
+		votes = Hash[finalists.product([0])]
+		@members.each do |member|
+			vote = finalists.sample
+			votes[vote] += 1
+			puts "#{member}: #{vote}"
+		end
+		return votes
+	end
+
+	def report_votes(final_votes)
+		final_votes.each do |finalist, votes|
+			puts "#{finalist}: #{votes}"
+		end
+	end
+
+	def announce_winner(final_votes)
+		final_votes.max_by { |finalist, votes| votes }.first
+	end
+
+end
