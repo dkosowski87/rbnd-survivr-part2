2683143dd581760ece5349048c36994c0b5856d3
Damian Kosowski <kosowski.d@gmail.com>
2016-06-17 19:12:32 +0200

    Implemented tribe class

---
+++ lib/tribe.rb
@@ -0,0 +1,25 @@
+class Tribe
+	attr_accessor :name, :members
+
+	def initialize name:, members:
+		@name = name
+		@members = members
+		print_tribe_creation_message
+	end
+
+	def to_s
+		@name
+	end
+
+	def tribal_council immune:
+		immune_member = @members.delete(immune)
+		eliminated = @members.sample
+		@members.push immune_member
+		@members.delete(eliminated)
+	end
+
+	private
+	def print_tribe_creation_message
+		puts "A new tribe has settled on the island. The tribes name - #{name}."
+	end
+end
